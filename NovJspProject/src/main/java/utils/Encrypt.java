336823c65ce7f4eba927c1ca7643eb39cd26bc5b
wwnOOv <145524959+wwnoov@users.noreply.github.com>
2023-11-22 08:59:12 +0900

    암호화 수정

--- NovJspProject/src/main/java/utils/Encrypt.java
+++ NovJspProject/src/main/java/utils/Encrypt.java
@@ -5,18 +5,17 @@ import java.security.NoSuchAlgorithmException;
 
 public class Encrypt {
     public static String getEncrypt(String pwd) {
-
+        //최종 비밀번호 생성
         String result = "";
+        //Salt 생성
         String salt = "";
         try {
             //1. SHA256 알고리즘 객체 생성
             MessageDigest md = MessageDigest.getInstance("SHA-256");
-
             //2. 비밀번호와 salt 합친 문자열에 SHA 256 적용
             md.update((pwd+salt).getBytes());
             byte[] pwdsalt = md.digest();
-
-            //3. byte To String (10진수의 문자열로 변경)
+            // byte To String (10진수의 문자열로 변경)
             StringBuffer sb = new StringBuffer();
             for (byte b : pwdsalt) {
                 sb.append(String.format("%02x", b));
