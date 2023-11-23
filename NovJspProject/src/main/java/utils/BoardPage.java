2523a93413c193484afa83dba7f2c3bd04940472
wwnOOv <145524959+wwnoov@users.noreply.github.com>
2023-11-23 22:52:38 +0900

    코드 수정

--- NovJspProject/src/main/java/utils/BoardPage.java
+++ NovJspProject/src/main/java/utils/BoardPage.java
@@ -5,10 +5,10 @@ public class BoardPage {
                                  int pageNum, String reqUrl) {
     String pagingStr = "";
 
-    // 단계 3 : 전체 페이지 수 계산
+    //  전체 페이지 수 계산
     int totalPages = (int) (Math.ceil(((double) totalCount / pageSize)));
 
-    // 단계 4 : '이전 페이지 블록 바로가기' 출력
+    //  '이전 페이지 블록 바로가기' 출력
     int pageTemp = (((pageNum - 1) / blockPage) * blockPage) + 1;
     if (pageTemp != 1) {
       pagingStr += "<a href='" + reqUrl + "?pageNum=1'>[첫 페이지]</a>";
@@ -17,7 +17,7 @@ public class BoardPage {
               + "'>[이전 블록]</a>";
     }
 
-    // 단계 5 : 각 페이지 번호 출력
+    // 각 페이지 번호 출력
     int blockCount = 1;
     while (blockCount <= blockPage && pageTemp <= totalPages) {
       if (pageTemp == pageNum) {
@@ -31,7 +31,7 @@ public class BoardPage {
       blockCount++;
     }
 
-    // 단계 6 : '다음 페이지 블록 바로가기' 출력
+    // 다음 페이지 블록 바로가기 출력
     if (pageTemp <= totalPages) {
       pagingStr += "<a href='" + reqUrl + "?pageNum=" + pageTemp
               + "'>[다음 블록]</a>";
